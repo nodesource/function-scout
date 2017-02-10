489e896d66456d394f0de54fd4b325dd0316f5f8
Thorsten Lorenz <thlorenz@gmx.de>
2017-02-10 17:41:00 -0500

    removing test code in main file

--- function-scout.js
+++ function-scout.js
@@ -55,23 +55,3 @@ module.exports = function functionScout(object) {
   const scouted = traverse(object).map(processNode)
   return { scouted, functions }
 }
-
-// Test
-// eslint-disable-next-line no-unused-vars
-function inspect(obj, depth) {
-  console.error(require('util').inspect(obj, false, depth || 5, true))
-}
-
-function ondata() { }
-function onerror() { }
-function onend() { }
-if (!module.parent && typeof window === 'undefined') {
-  const fs = require('fs')
-  const stream = fs.createReadStream(__filename)
-    .on('data', ondata)
-    .on('error', onerror)
-    .on('end', onend)
-
-  const res = module.exports(stream)
-  inspect(res)
-}
